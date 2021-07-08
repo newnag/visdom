244c6d3723290cbf657674bf144cd684c2693692
newnag <newnagtl@gmail.com>
2021-07-08 11:16:56 +0700

    0.1.2

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
+// ย่อขยายเมนูซ้าย
 function collap_aside(){
   document.querySelector('aside').classList.toggle('collap')
   document.querySelector('main').classList.toggle('collap')
   document.querySelector('main header').classList.toggle('collap')
 }
 
+// ค้นหาบน header
 function header_search(){
   document.querySelector('.search-icon .input-search').classList.toggle('show')
 }
 
+// ภาษา
 function header_lang(){
   document.querySelector('.lang .lang-menu').classList.toggle('show')
 }
